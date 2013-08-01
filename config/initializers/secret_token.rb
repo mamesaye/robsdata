d654db4b18d832ba237a05513409b398cd510f43
Adama <mamesaye@hotmail.com>
2013-08-01 11:20:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robsdata::Application.config.secret_token = '634cf07b2c25fd3e70e340402a7815950045d35141e4c3f2e90d15fbea9d10c91411cba9e125ffea1e1de7ca4c4080a97140d0742f348be15cc87d7deb7a4f74'
